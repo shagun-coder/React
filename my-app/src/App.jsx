a207d4d38b4cbcba30270f9f7ca1326f50ca4aa1
shagun-coder <shagunpadha108@gmail.com>
2026-03-21 18:01:51 +0530

    Initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <div id='chacha'>
+                <h1 id='child1'>hello</h1>
+                <h2 id='child2'> hello </h2>
+            </div>
+            <div id =' dad'></div>
+        </>
+        
+    );
+};
+
+export default App;
